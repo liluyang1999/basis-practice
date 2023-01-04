6fdd1b8d599ea3aa0c10a036fb3d88116ce69d36
Luyang Li <liluyang1999@163.com>
2023-01-04 16:54:22 +0800

    Practise basic grammar......

---
+++ src/Java38.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Java38 {
+
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("src.Java38");
+    }
+
+}
